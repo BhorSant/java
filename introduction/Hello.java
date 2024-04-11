0643ede83e05e0eba846ee1b9d5a3e666b7f552c
Santosh Bhor <91824340+BhorSant@users.noreply.github.com>
2024-04-11 17:27:53 +0530

    introduction

---
+++ introduction/Hello.java
@@ -0,0 +1,65 @@
+package introduction;
+class Hello
+{
+    public static void main(String args[])
+    {
+        // int num = 3;
+        // int num2 = 5;
+        // int result = num + num2;
+        // System.out.println(result);
+
+
+        // float marks = 6.5f;
+        // System.out.println(marks);
+
+        // marks = marks * 2;
+        // System.out.println(marks);
+        // char c = 'c';
+        // System.out.println(c);
+
+        // int num1 = 9;
+        // byte by = 127;
+        // short sh = 558;
+        // long l = 5854l;
+
+        // float f = 5.8f;
+        // double d = 5.8;
+        // boolean b = true;
+
+        // System.out.println(num1);
+        // System.out.println(by);
+        
+        // int n = 10_000;
+        // System.out.println(n);
+
+        // int and = 12;
+        // byte k = (byte) and ;
+        // System.out.println(k);
+
+        // float f = 5.6f;
+        // int t = (int) (f * 10);
+        // System.out.println(t);
+
+
+        // Operator
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(++a);
+        System.out.println(--a);
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(++a);
+        System.out.println(--a);
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(++a);
+    }
+
+}
