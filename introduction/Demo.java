c3eca8a705a2e9174599b5dab7b5cb93ab9104e5
Santosh Bhor <91824340+BhorSant@users.noreply.github.com>
2024-04-12 13:46:44 +0530

    loops created

--- introduction/Demo.java
+++ introduction/Demo.java
@@ -3,12 +3,89 @@ package introduction;
 public class Demo {
     
     public static void main(String[] args) {
-        int X = 6;
-        int Y = 6;
-        boolean result = X == Y;
-        System.out.println(result);
+        // int X = 6;
+        // int Y = 6;
+        // boolean result = X == Y;
+        // System.out.println(result);
 
-        boolean r = X != Y;
-        System.out.println(r);
+        // boolean r = X != Y;
+        // System.out.println(r);
+    
+
+        // int x = 110;
+        // if(x>10 && x<=10)
+        //     System.out.println("Hello");
+        // else
+        //     System.err.println("Bye");
+
+
+        // int y = 5;
+        // int z = 7;
+        // if (y>z)
+        //     System.out.println(y);
+        // else
+        //     System.out.println(z);
+
+        // int a = 5;
+        // int b = 7;
+        // int c = 9;
+        // if (a>b)
+        //     System.out.println(a);
+        // else if (b>c)
+        //     System.out.println(c);
+        // else
+        //     System.out.println(b);
+
+
+        // int n = 4;
+        // int result =0;
+        // // if (n%2==0)
+        // //  result = 10;
+        // // else
+        // //  result = 15;
+        // // System.out.println(result);
+        // result = n%2==0 ? 10:20;
+        // System.out.println(result);
+        // int n = 7;
+        // switch(n) {
+            
+        //     case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 3:
+        //         System.out.println("Wednesday");
+        //         break;
+        //     case 4:
+        //         System.out.println("Thursday");
+        //         break;
+        //     case 5:
+        //         System.out.println("Friday");
+        //         break;
+        //     case 6:
+        //         System.out.println("Saturday");
+        //         break;
+        //     case 7:
+        //         System.out.println("Sunday");
+        //         break;
+        //     default:
+        //         System.out.println("Enter the valid number");
+
+
+
+            
+            // ### Loops 
+
+
+        while (true) {
+            System.out.println("Hello");
+            }
+        }
+
+        
+
+       
     }
 }
