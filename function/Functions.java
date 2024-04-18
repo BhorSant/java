98672c12ada99ebacb2b3d3dbad752ac4bcf012b
Santosh Bhor <91824340+BhorSant@users.noreply.github.com>
2024-04-18 17:02:58 +0530

    function added

---
+++ function/Functions.java
@@ -0,0 +1,14 @@
+package function;
+
+public class Functions {
+    public static void main(String[] args) {
+        System.out.println("In main Methods");
+        greeting();
+        System.out.println("Method Calling complete");
+        greeting();
+
+    }
+    public static void greeting(){
+        System.out.println("Good morning from KGCoding");
+    }
+}
